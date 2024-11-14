e5e3a40d13518681c9ea4844354de43854d70671
GabSichiroli <Sichiroli07@gmail.com>
2024-11-14 10:57:52 -0300

    commit

--- Projeto Final/salvando caso de merda tmj.asm
+++ Projeto Final/salvando caso de merda tmj.asm
@@ -41,23 +41,23 @@ LIMPA_TELA MACRO
 
     ;Move Cursor
                MOV AH, 02H
-               MOV BH, 0        ;Coluna
-               MOV DH, 5        ;Linha
+               MOV BH, 0        
+               MOV DH, 10        ;Linha
                MOV DL, 0
                INT 10H
 ENDM
-IMPRIME_ESPACOS MACRO QUANTIDADE
-                    LOCAL CONTAESPACOS
-                    PUSH  CX                 ; Salva o valor original de CX
-                    MOV   CX, &QUANTIDADE    ; Usa o valor do parâmetro QUANTIDADE
-    CONTAESPACOS:   
-                    MOV   DL, 20H            ; Caractere de espaço (ASCII 32)
-                    MOV   AH, 02H            ; Função para saída de caractere
-                    INT   21H                ; Interrupção para imprimir DL
-                    LOOP  CONTAESPACOS       ; Decrementa CX e repete até CX = 0
-                    POP   CX                 ; Restaura o valor original de CX
+Espaso MACRO QUANTIDADE
+Local CONTAESPASOS, END_SPACES
+                 PUSH CX
+                 MOV  CX, QUANTIDADE
+    CONTAESPASOS:
+                 MOV  DL, 20H
+                 MOV  AH,02
+                 INT  21h
+                 LOOP CONTAESPASOS
+    END_SPACES:  
+                 POP  CX
 ENDM
-
 SALVAMJOGO MACRO
                PUSH BX
                PUSH SI
@@ -69,7 +69,7 @@ VOLTAVALOR MACRO
                POP DI
 ENDM
 .DATA
-    MATRIZMAPA   DW 0,0,0,0,0,0,1,1,1,0     ,     1,1,1,0,0,0,0,0,0,0
+    MATRIZ       DW 0,0,0,0,0,0,1,1,1,0     ,     1,1,1,0,0,0,0,0,0,0
                  DW 0,0,1,0,0,0,0,1,0,0     ,     0,0,0,0,1,0,0,0,0,0
                  DW 0,0,1,0,0,0,0,0,0,0     ,     0,0,0,1,1,0,0,0,0,0
                  DW 0,0,0,0,0,0,0,1,0,0     ,     0,0,0,0,1,0,1,1,0,0
@@ -91,8 +91,6 @@ ENDM
                  DW 3,3,3,3,3,3,3,3,3,3     ,     4,4,4,4,4,4,4,4,4,4
                  DW 3,3,3,3,3,3,3,3,3,3     ,     4,4,4,4,4,4,4,4,4,4
 
-MATRIZIMPRESSÃO DW 10 DUP(10 DUP('~'))
-
 
     VETOR        DB 10 DUP (0)
     ;Mensagens;
@@ -104,6 +102,7 @@ MATRIZIMPRESSÃO DW 10 DUP(10 DUP('~'))
     LOGO5        DB 13,10,'              ===================================================              ', 13,10, '$'
     ENTMSG1      DB 13,10,'              Insira ate 3 numeros de 0-9 para iniciar o jogo:', '$'
     ;Mensagens de controle;
+    ;Mensagens de controle;
     ;Remover em Projeto Final;
     MSGCONTROLE  DB 13,10,'OK1', '$'
     MSGCONTROLE1 DB 13,10,'OK2', '$'
@@ -112,85 +111,106 @@ MATRIZIMPRESSÃO DW 10 DUP(10 DUP('~'))
 .CODE
 MAIN PROC
     ;Acesso ao DATA
-                     MOV         AX, @DATA
-                     MOV         DS,AX
+                  MOV               AX, @DATA
+                  MOV               DS,AX
 
     ;Chamadas;
-                     CALL        INICIAR
+                  CALL              INICIAR
 
     ;Termina o programa
-                     MOV         AH,4CH
-                     INT         21H
+                  MOV               AH,4CH
+                  INT               21H
 MAIN ENDP
 INICIAR PROC
-                     LIMPA_TELA
-                     IMPMENSAG   LOGO1
-                     IMPMENSAG   LOGO2
-                     IMPMENSAG   LOGO3
-                     IMPMENSAG   LOGO4
-                     IMPMENSAG   LOGO5
-                     IMPMENSAG   ENTMSG1
+                  LIMPA_TELA
+                  IMPMENSAG         LOGO1
+                  IMPMENSAG         LOGO2
+                  IMPMENSAG         LOGO3
+                  IMPMENSAG         LOGO4
+                  IMPMENSAG         LOGO5
+                  IMPMENSAG         ENTMSG1
 
-                     MOV         CX, 3
-                     XOR         BX,BX
-                     XOR         DX,DX
-                     MOV         AH,1
+                  MOV               CX, 3
+                  XOR               BX,BX
+                  XOR               DX,DX
+                  MOV               AH,1
 
-    LerEnt:          
-                     INT         21h
-                     CMP         AL, 0DH
-                     JE          CompENT
-                     MOV         DL,AL
-                     ADD         BL, DL
-                     LOOP        LerEnt
-                     LIMPA_TELA
+    LerEnt:       
+                  INT               21h
+                  CMP               AL, 0DH
+                  JE                CompENT
+                  MOV               DL,AL
+                  ADD               BL, DL
+                  LOOP              LerEnt
+                  LIMPA_TELA
     ;Aqui ó - Gabi;
-    CompENT:         
-                     CMP         BL, 4
-                     JB          RetornaEnt
+    CompENT:      
+                  CMP               BL, 4
+                  JB                RetornaEnt
 
-    DivDerminadaM:   
-                     MOV         AX, BX
-                     MOV         BL, 4
-                     DIV         BL
+    DivDerminadaM:
+                  MOV               AX, BX
+                  MOV               BL, 4
+                  DIV               BL
 
-    CompQUA:         
-                     CMP         AH, 1
-                     JE          QUA1
+    CompQUA:      
+                  CMP               AH, 1
+                  JE                QUA1
 
-                     CMP         AH, 2
-                     JE          QUA2
+                  CMP               AH, 2
+                  JE                QUA2
 
-                     CMP         AH, 3
-                     JE          QUA3
+                  CMP               AH, 3
+                  JE                QUA3
                       
-                     CMP         AH, 0
-                     JE          QUA4
+                  CMP               AH, 0
+                  JE                QUA4
 
     ;Vai determinar o quadrante ultilizado e definir o zerado;
-    QUA1:            
-                     INFORMATRIZ 0,0,360
-                     CALL        QUARDANTEJOGO
-                     JMP         RetornaEnt
-    QUA2:            
-                     INFORMATRIZ 20,0,360
-                     CALL        QUARDANTEJOGO
-                     JMP         RetornaEnt
-    QUA3:            
-                     INFORMATRIZ 0,400,760
-                     CALL        QUARDANTEJOGO
-                     JMP         RetornaEnt
+    QUA1:         
+                  INFORMATRIZ       0,0,360
+                  CALL              QUARDANTEJOGO
+                  JMP               RetornaEnt
+    QUA2:         
+                  INFORMATRIZ       20,0,360
+                  CALL              QUARDANTEJOGO
+                  JMP               RetornaEnt
+    QUA3:         
+                  INFORMATRIZ       0,400,760
+                  CALL              QUARDANTEJOGO
+                  JMP               RetornaEnt
 
-    QUA4:            
-                     INFORMATRIZ 20,400,760
-                     CALL        QUARDANTEJOGO
-                     JMP         RetornaEnt
+    QUA4:         
+                  INFORMATRIZ       20,400,760
+                  CALL              QUARDANTEJOGO
+                  JMP               RetornaEnt
 
-    RetornaEnt:      
-                     RET
+    RetornaEnt:   
+                  RET
 INICIAR ENDP
 QUARDANTEJOGO PROC
-                     SALVAMJOGO
-                     RET
+                  SALVAMJOGO
+                  MOV               CX,10
+                  
+                  JMP               L1
+    MudaLinha:    
+                  SUB               BX,20
+                  pula_linha                  
+                  ADD               SI,40                  
+                  MOV               CX,10                  
+                  CMP               SI,DI
+                  JG                Retornaimp
+    L1:                       
+                  MOV               AH, 02H
+    IMPRIMELINHA: 
+                  MOV               DX, MATRIZ [SI][BX]    
+                  OR                DL,30H                 
+                  INT               21H
+                  ADD               BX,2
+                  LOOP              IMPRIMELINHA
+                  Espaso 15
+                  JMP               MudaLinha
+    Retornaimp:   
+                  RET
 QUARDANTEJOGO ENDP
 END MAIN
